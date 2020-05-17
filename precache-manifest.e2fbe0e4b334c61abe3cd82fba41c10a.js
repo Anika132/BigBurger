17cb23a4dec651652f259207e0b2a980757f4a96
Anika <anikasharma132@gmail.com>
2020-05-17 14:55:46 +0530

    Updates

---
+++ precache-manifest.e2fbe0e4b334c61abe3cd82fba41c10a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99d4e4da58211c4a53a92274c52be29",
+    "url": "/BigBurger/index.html"
+  },
+  {
+    "revision": "6cfd41763bad5fe9aa42",
+    "url": "/BigBurger/static/css/3.9c56c590.chunk.css"
+  },
+  {
+    "revision": "5a5f4bfddab3ea192dbb",
+    "url": "/BigBurger/static/css/4.3adef230.chunk.css"
+  },
+  {
+    "revision": "ca3180a35f32c0f9000f",
+    "url": "/BigBurger/static/css/5.06a2488c.chunk.css"
+  },
+  {
+    "revision": "e49b1cb3fe1a59d3bbf0",
+    "url": "/BigBurger/static/css/main.5596eb79.chunk.css"
+  },
+  {
+    "revision": "b6fd3d681648568198c9",
+    "url": "/BigBurger/static/js/2.c6a9c019.chunk.js"
+  },
+  {
+    "revision": "6cfd41763bad5fe9aa42",
+    "url": "/BigBurger/static/js/3.92c4d580.chunk.js"
+  },
+  {
+    "revision": "5a5f4bfddab3ea192dbb",
+    "url": "/BigBurger/static/js/4.0a021e30.chunk.js"
+  },
+  {
+    "revision": "ca3180a35f32c0f9000f",
+    "url": "/BigBurger/static/js/5.a62c17ef.chunk.js"
+  },
+  {
+    "revision": "e49b1cb3fe1a59d3bbf0",
+    "url": "/BigBurger/static/js/main.35a13e67.chunk.js"
+  },
+  {
+    "revision": "a27df19a51688028e6bb",
+    "url": "/BigBurger/static/js/runtime~main.9d156434.js"
+  },
+  {
+    "revision": "ad3b151202adcc82aa1ca2dafa760a4f",
+    "url": "/BigBurger/static/media/404.ad3b1512.gif"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BigBurger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "89700c8f6da5a0a1624db1983b06590b",
+    "url": "/BigBurger/static/media/loading.89700c8f.gif"
+  }
+]);
